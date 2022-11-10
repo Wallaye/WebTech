1da7b60cd01d7eb657e7f0fa9532fc9f3280abbd
Wallaye <pavel.vashkevich03@gmail.com>
2022-11-10 22:27:03 +0300

    Added DAO

--- Lab2/Proj/src/main/java/by/tc/task01/dao/ApplianceDAO.java
+++ Lab2/Proj/src/main/java/by/tc/task01/dao/ApplianceDAO.java
@@ -3,6 +3,14 @@ package by.tc.task01.dao;
 import by.tc.task01.entity.Appliance;
 import by.tc.task01.entity.criteria.Criteria;
 
+/**
+ * Represents appliance dao
+ */
 public interface ApplianceDAO {
-	Appliance find(Criteria criteria);
+	/**
+	 * Finds all appliances by given search criteria
+	 * @param criteria search criteria
+	 * @return array of appliances that satisfy search criteria
+	 */
+	Appliance[] find(Criteria criteria);
 }
--- Lab2/Proj/src/main/java/by/tc/task01/entity/criteria/Criteria.java
+++ Lab2/Proj/src/main/java/by/tc/task01/entity/criteria/Criteria.java
@@ -3,23 +3,45 @@ package by.tc.task01.entity.criteria;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents search criteria for specific appliance type
+ */
 public class Criteria {
 
 	private String groupSearchName;
-	private Map<String, Object> criteria = new HashMap<String, Object>();
+	private Map<String, Object> criteria = new HashMap<>();
 
+	/**
+	 * Instantiates object of a type Criteria
+	 * @param groupSearchName name searching type
+	 */
 	public Criteria(String groupSearchName) {
 		this.groupSearchName = groupSearchName;
 	}
-	
+
+	/**
+	 * Gets the search appliance type name
+	 * @return search appliance type name
+	 */
 	public String getGroupSearchName() {
 		return groupSearchName;
 	}
 
+	/**
+	 * Add new search criteria
+	 * @param searchCriteria field to search
+	 * @param value value of search criteria
+	 */
 	public void add(String searchCriteria, Object value) {
 		criteria.put(searchCriteria, value);
 	}
-	
-	// you may add your own code here
+
+	/**
+	 * Gets the search criteria
+	 * @return search criteria
+	 */
+	public Map<String, Object> getSearchCriteria() {
+		return criteria;
+	}
 
 }
